cddcc189533abb14917fa4f385689338cdd7f09b
mohnish-07 <mohnishpvt07@gmail.com>
2025-03-02 22:35:12 +0530

    Add files via upload

---
+++ src/main/java/com/project/snackswipe/model/UserLoginRequest.java
@@ -0,0 +1,66 @@
+package com.project.snackswipe.model;
+
+import org.apache.commons.codec.binary.Base64;
+
+
+
+public class UserLoginRequest 
+{
+	private String userType;
+	private String prnOrEmployeeId;
+	private String name;
+	private String password;
+	
+	
+	public UserLoginRequest()
+	{
+		
+	}
+	/**
+	 * @return the prnOrEmployeeId
+	 */
+	public String getPrnOrEmployeeId() {
+		return prnOrEmployeeId;
+	}
+	/**
+	 * @param prnOrEmployeeId the prnOrEmployeeId to set
+	 */
+	public void setPrnOrEmployeeId(String prnOrEmployeeId) {
+		this.prnOrEmployeeId = prnOrEmployeeId;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+		
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = Base64.encodeBase64String(password.getBytes());
+	}
+	/**
+	 * @return the userType
+	 */
+	public String getUserType() {
+		return userType;
+	}
+	/**
+	 * @param userType the userType to set
+	 */
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
